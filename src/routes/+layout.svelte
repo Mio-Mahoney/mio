1d5c01bbe206d4ade7051c3056b3b4fdeb8df978
Mio Mahoney <mmahone5@oswego.edu>
2024-11-11 12:59:38 -0500

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import "../app.css";
+	let { children } = $props();
+	export const prerender = true;
+	export const ssr = true;
+	export const trailingSlash = "ignore";
+</script>
+
+{@render children()}
---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,5 @@
+<h1
+    class="text-sky-600 border-spacing-6 border-green-500 h-screen w-screen justify-center text-center flex items-center text-6xl"
+>
+    Hello Welcome to my site
+</h1>
