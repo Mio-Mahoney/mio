58f04f2c18969a37c69280a9cb5d551b92e0ed5a
Mio Mahoney <mmahone5@oswego.edu>
2024-11-11 14:22:03 -0500

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import "../app.css";
 	let { children } = $props();
-	export const prerender = true;
-	export const ssr = true;
-	export const trailingSlash = "ignore";
 </script>
 
 {@render children()}
