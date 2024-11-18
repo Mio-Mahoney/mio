5b8c711e44a246d6b39eae4f4ff9c8a53bf389cd
Mio Mahoney <mmahone5@oswego.edu>
2024-11-18 08:20:06 -0500

    updated header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Header from "$lib/headerLink.svelte";
+</script>
+
+<main>
+    <nav class="text-center justify-center text-3xl p-5 space-x-5 h-5 flex">
+        <Header href="/">Home</Header>
+        <Header href="/resume">Resume</Header>
+        <Header href="/about-me">About Me</Header>
+    </nav>
+
+    <h1 >Welcome to the page</h1>
+</main>
